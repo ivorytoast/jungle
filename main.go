d72203657c47ed8228318af8ece73132aa1170d5
ivorytoast <anthonyhamill3853@gmail.com>
2024-01-05 22:31:49 -0500

    First Commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+
+	"net/http"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.GET("/healthz", func(c echo.Context) error {
+		return c.String(http.StatusOK, "UP!")
+	})
+	e.Logger.Fatal(e.Start(":7500"))
+}
